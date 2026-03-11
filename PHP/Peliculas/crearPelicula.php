bf56cb4cb211d7a2a35e51e91dcbfa2b2d79c094
Axel <axel.hernandez@educa.madrid.com>
2026-03-11 23:58:09 +0100

    Añadi mas codigo al html

--- PHP/Peliculas/crearPelicula.php
+++ PHP/Peliculas/crearPelicula.php
@@ -1,11 +1,26 @@
 <?php
-require('db.php');
+require('../Conexiondb.php');
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+   //aquí empieza el insert 
+  $stmt = $conn->prepare("INSERT INTO `pelis`
+  (`id`, `titulo`, `genero`, `duracion`, `descripcion`, `anio`,`estrellas`,`director`) 
+  VALUES (NULL, :titulo, :genero, :duracion, :descripcion, :anio , :estrellas, :director)");
+  $stmt->bindParam(':titulo', $_POST['titulo']);
+  $stmt->bindParam(':genero', $_POST['genero']);
+  $stmt->bindParam(':duracion', $_POST['duracion']);
+  $stmt->bindParam(':descripcion', $_POST['descripcion']);
+  $stmt->bindParam(':año', $_POST['año']);
+  $stmt->bindParam(':estrellas', $_POST['estrellas']);
+  $stmt->bindParam(':director', $_POST['director']);
+
+
+  $stmt->execute();
+  header("Location: /html/registro.php");//< -- CAMBAIR LA RUTA DEL JS
 
 echo "Conexión exitosa";
 } catch(PDOException $e) {
--- PHP/Peliculas/editarPelicula.php
+++ PHP/Peliculas/editarPelicula.php
@@ -6,6 +6,22 @@ try {
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+  
+  //aquí empieza el insert 
+  $stmt = $conn->prepare("UPDATE `pelis` SET `titulo`=:titulo,`genero`= :genero,`duracion`=:duracion,`descripcion`=:descripcion,
+  `año`=:año,`estrellas`=:estrellas,`director`=:director WHERE `pelis`.`id` = :id");
+  $stmt->bindParam(':titulo', $_POST['titulo']);
+  $stmt->bindParam(':genero', $_POST['genero']);
+  $stmt->bindParam(':duracion', $_POST['duracion']);
+  $stmt->bindParam(':descripcion', $_POST['descripcion']);
+  $stmt->bindParam(':año', $_POST['año']);
+  $stmt->bindParam(':estrellas', $_POST['estrellas']);
+  $stmt->bindParam(':director', $_POST['director']);
+  $stmt->bindParam(':id', $_POST['id']);
+  // use exec() because no results are returned
+  $stmt->execute();
+  header("Location: .php");//lanzar un mensaje de confirmacion en javaSrcipt
+
 
 echo "Conexión exitosa";
 } catch(PDOException $e) {
