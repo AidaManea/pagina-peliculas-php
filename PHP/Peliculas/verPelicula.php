df3f4c010bc3994b235ab62a3375a3be94b91e06
Axel <axel.hernandez@educa.madrid.com>
2026-03-12 23:58:57 +0100

    cmabios

--- PHP/Peliculas/verPelicula.php
+++ PHP/Peliculas/verPelicula.php
@@ -1,11 +1,12 @@
 <?php
-require('db.php');
+require('Conexiondb.php');
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+  $sql = "SELECT * FROM pelis";
+  $stmt = $conn->query($sql);
+  $peliculas = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 echo "Conexión exitosa";
 } catch(PDOException $e) {
@@ -13,3 +14,44 @@ echo "Conexión exitosa";
 }
 
 ?>
+<h2>Lista de Películas</h2>
+
+<a href="../../html/AñadirPelicula.html">Añadir película</a>
+
+<table borde="1">
+
+<tr>
+<th>Titulo</th>
+<th>Genero</th>
+<th>Duracion</th>
+<th>Año</th>
+<th>Director</th>
+<th>Acciones</th>
+</tr>
+
+<?php foreach($peliculas as $p): ?>
+
+<tr>
+
+<td><?= $p['titulo'] ?></td>
+<td><?= $p['genero'] ?></td>
+<td><?= $p['duracion'] ?></td>
+<td><?= $p['anio'] ?></td>
+<td><?= $p['director'] ?></td>
+
+<td>
+
+<a href="editarPelicula.php?id=<?= $p['id'] ?>">Editar</a>
+
+<a href="eliminarPelicula.php?id=<?= $p['id'] ?>" 
+onclick="return confirm('¿Seguro que quieres borrar esta película?')">
+Eliminar
+</a>
+
+</td>
+
+</tr>
+
+<?php endforeach; ?>
+
+</table>
